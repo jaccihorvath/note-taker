bd1d3c64470179ea32168052ee2ebc2a6d275d1c
Jacci Horvath <jaccinhorvath@gmail.com>
2021-01-11 12:40:46 -0500

    trying to fix routes

--- routes/html.js
+++ routes/html.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 // routes
 module.exports = function(app) {
-    // homepage
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-
     // notes
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+    
+    // homepage
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
