14597287463bb16cc341a3867a2c55fd90a4844f
Jacci Horvath <jaccinhorvath@gmail.com>
2021-01-11 11:49:39 -0500

    fixing path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // dependencies
 const express = require('express');
 const path = require('path');
-const db = require('\db\db.json')
+const db = require('./db/db.json')
 const uniqid = require('uniqid');
 
 // express
