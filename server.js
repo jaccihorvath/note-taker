38969a16760d9bacd7fcc24ecfa87ed610e9d72e
Jacci Horvath <jaccinhorvath@gmail.com>
2021-01-11 12:29:17 -0500

    adding access to public

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(__dirname + "./public"));
 
 // routes
 require('./routes/api')(app);
